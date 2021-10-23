8c369e645118d64d5f54ae7752fbe0b58903496c
Swiss-Frank <61509331+Swiss-Frank@users.noreply.github.com>
2021-10-23 02:29:26 +0100

    Update Exercise2.js

--- Exercise2.js
+++ Exercise2.js
@@ -1,8 +1,19 @@
-const containers = ["Fish", "Fish", "Fish" , "Coal", "Coal", "Coal"];
-var waitingTime;
-waitingTime = 2;
+let container =["Fish","Fish","Fish","Coal","Coal","Coal","Honey","Honey"]; 
 
-containers.push("Eggs");
+function loadingCrane() {
+    var M;
+    
+    for(M = 1; M < 2; M++) {
+        container.sort();
+        console.log(container.length);
+        console.log("")
+        console.log(container);
+    }
+    return M.valueOf(container);    
 
-containers.length();
-containerCrane();
+}
+
+loadingCrane();
+
+//There are 0 units of time between any of the two same container types
+//Coal -> Coal -> Coal -> Fish -> Fish -> Fish -> Honey -> Honey
